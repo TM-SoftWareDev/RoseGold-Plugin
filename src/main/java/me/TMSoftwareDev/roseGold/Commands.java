30750a7cc3e19c90aa3309422564e5ee57e45b30
Tim <timmail908@gmail.com>
2025-10-18 11:44:22 +0100

    Refactor: moved Commands into Commands Class

--- src/main/java/me/TMSoftwareDev/roseGold/Commands.java
+++ src/main/java/me/TMSoftwareDev/roseGold/Commands.java
@@ -6,17 +6,41 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 
 public class Commands implements CommandExecutor {
 
+    private final HashMap<UUID, Long> cooldown;
+
+    public Commands() {
+        this.cooldown = new HashMap<>();
+    }
+
     @Override
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("Reset")) {
             if (sender instanceof Player p) {
-
                 p = (Player) sender;
-                p.setHealth(0.0);
-                p.sendMessage(ChatColor.RED + "You Have Opted to Die");
+                if (!this.cooldown.containsKey(p.getUniqueId())) {
+                    this.cooldown.put(p.getUniqueId(), System.currentTimeMillis());
+                    p.setHealth(0.0);
+                    p.sendMessage(ChatColor.RED + "You Have Opted to Die");
+                } else {
+                    long TimeElapsed = System.currentTimeMillis() - cooldown.get(p.getUniqueId());
+
+                    if (TimeElapsed >= 10000) {
+                        this.cooldown.put(p.getUniqueId(), System.currentTimeMillis());
+                        p.setHealth(0.0);
+                        p.sendMessage(ChatColor.RED + "You Have Opted to Die");
+                    } else {
+                        p.sendMessage("You Cant Reset for another " + (10000 - TimeElapsed) + " Milliseconds");
+                    }
+                }
+
+
             }
 
         }
