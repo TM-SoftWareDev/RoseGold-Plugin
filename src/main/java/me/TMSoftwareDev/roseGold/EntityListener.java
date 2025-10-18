b94e9708ebb6267b82db603d931a2330b0f08be8
Tim <timmail908@gmail.com>
2025-10-18 10:24:01 +0100

    Refactor: moved PlayerJoin into EnityListener Class Added: Command reset -  Kills Player

--- src/main/java/me/TMSoftwareDev/roseGold/EntityListener.java
+++ src/main/java/me/TMSoftwareDev/roseGold/EntityListener.java
@@ -1,8 +1,23 @@
 package me.TMSoftwareDev.roseGold;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EntityListener implements Listener {
+
+    @EventHandler
+    public void OnPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String name = player.getName();
+        if (name.equals("TalllTim")) {
+            event.setJoinMessage(ChatColor.BLUE + "" + ChatColor.BOLD + "Hello Divine King of True Glory and Noble Status Tim");
+        } else if (name.equals("airhopman")) {
+            event.setJoinMessage(ChatColor.RED + "Hello Cheater");
+        }
+    }
 
 
 }
--- src/main/java/me/TMSoftwareDev/roseGold/RoseGold.java
+++ src/main/java/me/TMSoftwareDev/roseGold/RoseGold.java
@@ -1,9 +1,10 @@
 package me.TMSoftwareDev.roseGold;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RoseGold extends JavaPlugin implements Listener {
@@ -11,7 +12,7 @@ public final class RoseGold extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         System.out.println("@ RoseGold Enabled");
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new EntityListener(), this);
     }
 
     @Override
@@ -19,14 +20,17 @@ public final class RoseGold extends JavaPlugin implements Listener {
         System.out.println("@ RoseGold Disabled");
     }
 
-    @EventHandler
-    public void OnPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        String name = player.getName();
-        if (name.equals("TalllTim")) {
-            event.setJoinMessage("Hello Divine King of True Glory and Noble Status Tim");
-        } else if (name.equals("airhopman")) {
-            event.setJoinMessage("Hello Cheater");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("Reset")) {
+            if (sender instanceof Player p) {
+
+                p = (Player) sender;
+                p.setHealth(0.0);
+                p.sendMessage(ChatColor.RED + "You Have Opted to Die");
+            }
+
         }
+        return true;
     }
 }
