8c3134db6f44ed3057ee841aca4d03f33c8589f9
Tim <timmail908@gmail.com>
2025-10-19 19:47:11 +0100

    Added: Gauntlets Of Rage Added: Blaze Stick

--- src/main/java/me/TMSoftwareDev/roseGold/EntityListener.java
+++ src/main/java/me/TMSoftwareDev/roseGold/EntityListener.java
@@ -44,7 +44,7 @@ public class EntityListener implements Listener {
     public void OnPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         String name = player.getName();
-        player.getInventory().addItem(Items.getGauntletsOfRage());
+        
         if (name.equals("TalllTim")) {
             event.setJoinMessage(ChatColor.BLUE + "" + ChatColor.BOLD + "Hello Divine King of True Glory and Noble Status Tim");
         } else if (name.equals("airhopman")) {
